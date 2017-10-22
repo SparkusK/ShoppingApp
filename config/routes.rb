9ce5a428af9eec0ec095982a65b5621592f89b74
Shane Kelly <shane.sparky.kelly@gmail.com>
2017-10-22 17:47:32 +0200

    Revert "Remove redis/dashing stuff from cable.yml and routes.rb"
    This reverts commit b158d0244f807f288831f3d15b56e84ae89eb0b9.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  mount Dashing::Engine, at: Dashing.config.engine_path
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
 
